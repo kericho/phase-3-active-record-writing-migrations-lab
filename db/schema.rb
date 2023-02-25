c5960d4ace3bc5582083a50a8e66101ce5b5b657
¨kericho¨ <lvisrono90@gmail.com>
2023-02-25 11:25:16 +0300

    completed assignment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_24_192312) do
+ActiveRecord::Schema.define(version: 3) do
 
   create_table "students", force: :cascade do |t|
     t.string "name"
